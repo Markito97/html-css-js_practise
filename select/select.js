9440dad187f27f26f37b5019222cfa0c032e5faa
Markito97 <jetblade0@gmail.com>
2022-11-14 17:45:11 +0300

    Something is working

--- select/select.js
+++ select/select.js
@@ -1,36 +1,36 @@
 const customDropDown = document.querySelector(".customDropDown");
-const dropDown = document.querySelector(".dropDown");
+const dropDownTextContent = document.querySelector(".dropDownTextContent");
 const chooseOption = document.querySelector(".chooseOption");
 const dropDownContent = document.querySelector(".dropDownContent");
 const content = document.querySelector(".content");
+const onToggleBtn = document.querySelector(".chooseOptionBtn");
 
-customDropDown.addEventListener("click", (e) => {
-  chooseOption.style.transform =
-    "translateY(-20px) translateX(0px) scale(0.85)";
-  chooseOption.style.color = "#46abfd";
-  customDropDown.style.border = "3px solid #46abfd";
-  dropDownContent.style.display = "block";
-});
+let toggle = false;
+const onToggle = (e) => {
+  const isShow = toggle ? (toggle = false) : (toggle = true);
+  if (
+    (isShow && e.target.className === "chooseOption") ||
+    (e.target.id === "chooseBtn" && isShow)
+  ) {
+    dropDownContent.style.display = "block";
+    chooseOption.style.transform = "translateY(-25px) scale(0.85)";
+    chooseOption.style.color = "#46abfd";
+    customDropDown.style.border = "3px solid #46abfd";
+  } else if (dropDownTextContent.textContent === "") {
+    dropDownContent.style.display = "none";
+    chooseOption.style.transform = "translateX(0px)";
+    chooseOption.style.color = "rgb(118, 118, 118)";
+    customDropDown.style.border = "3px solid rgb(118, 118, 118)";
+  } else {
+    dropDownContent.style.display = "none";
+  }
+};
 
 content.addEventListener("click", (e) => {
   if (e.target.className === "dropDownItem") {
-    dropDown.innerHTML = e.target.textContent;
-    // if (dropDown.textContent !== "") {
-    //     // chooseOption.style.transform = "translateY(0px)" // поиграться с этим
-    //     // dropDown.innerHTML === e.target.textContent
-    // }
-  }
-  if (
-    e.target.className !== "chooseOption" &&
-    e.target.className !== "customDropDown"
-  ) {
-    customDropDown.style.border = "3px solid rgb(118, 118, 118)";
-    chooseOption.style.color = "black";
-    if (dropDown.innerHTML === "") {
-      chooseOption.style.transform = "translateY(0px)";
-      dropDownContent.style.display = "none";
-    } else {
-      dropDownContent.style.display = "none";
-    }
+    dropDownTextContent.innerHTML = e.target.textContent;
+    chooseOption.style.transform = "translateY(-25px) scale(0.85)";
   }
 });
+
+customDropDown.addEventListener("click", onToggle, false);
